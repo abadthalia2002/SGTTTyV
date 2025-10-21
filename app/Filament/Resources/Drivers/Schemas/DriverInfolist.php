c1d8d1c6062fd533c9242568fd02d696492093af
cesarcunyarache <cesarcunyarache@gmail.com>
2025-10-21 14:29:32 -0500

    feat: add Filament resources for drivers and vehicles management

---
+++ app/Filament/Resources/Drivers/Schemas/DriverInfolist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Drivers\Schemas;
+
+use Filament\Schemas\Schema;
+
+class DriverInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
